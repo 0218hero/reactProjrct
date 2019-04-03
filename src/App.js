ad2a2ad6ece33f3f637becada83ab016789fe02a
wotesen <wotesen@gmail.com>
2019-04-03 10:58:44 +0800

    'day01'

---
+++ src/App.js
@@ -0,0 +1,16 @@
+/**
+ * Created by 满眼韶华 on 2019/4/3.
+ */
+import React, {Component} from 'react'
+/*
+ 应用根组件
+ */
+class App extends Component {
+  render() {
+    return (
+      <div>App</div>
+    )
+  }
+}
+
+export default App
---
+++ src/index.js
@@ -0,0 +1,9 @@
+/**
+ * Created by 满眼韶华 on 2019/4/3.
+ */
+import React from 'react'
+import ReactDOM from 'react-dom'
+import {Button} from 'antd'
+
+import App from './App'
+ReactDOM.render(<App />, document.getElementById('root'))
